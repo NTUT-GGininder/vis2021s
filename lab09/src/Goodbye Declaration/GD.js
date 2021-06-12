05f4fd9cdd1ca4b8c3f9ccbc5ab0b1a41ae7025b
106590013 <t106590013@ntut.org.tw>
2021-06-12 17:58:22 +0800

    lab9 add 2-music

---
+++ lab09/src/Goodbye Declaration/GD.js
@@ -0,0 +1,187 @@
+var SN=[];
+var verse=[];
+var chorus=[]
+
+var console = {};
+console.log = function(){};
+
+var isEmpty = function(data) {
+    if (typeof(data) === 'object') {
+      if (JSON.stringify(data) === '{}' || JSON.stringify(data) === '[]') {
+        return true;
+      } else if (!data) {
+        return true;
+      }
+      return false;
+    } else if (typeof(data) === 'string') {
+      if (!data.trim()) {
+        return true;
+      }
+      return false;
+    } else if (typeof(data) === 'undefined') {
+      return true;
+    } else {
+      return false;
+    }
+}
+  
+
+function read(Arr, src){
+    d3.text(src).then(function(data) {
+        console.log(data);
+        let parsedCSV = d3.csvParseRows(data);
+        let key;
+        let begin;
+        let duration;
+        let end;
+        let subtitle = '';
+        let x;
+        let y;
+        let width;
+        let height;
+        let color;
+        let strokeColor;
+        let writing_mode;
+        let rotate=null;
+        let flag = false;
+        let fontSize;
+        console.log(parsedCSV);
+        function initP(){
+            key=null;
+            begin=null;
+            duration=null;
+            end=null;
+            subtitle = '';
+            x=null;
+            y=null;
+            width=null;
+            height=null;
+            fontSize=null;
+            color=null;
+            strokeColor=null;
+            writing_mode=0;
+            rotate=null;
+            flag = false;
+        }
+        d3.select('body')
+            .data(parsedCSV)
+            .enter()
+            .text(function(d) {
+                if (!isNaN(d) && d != '') {
+                    key = parseInt(d) - 1;
+                } else if (d == '') {
+                    Arr.push({
+                        'key': key,
+                        'begin': begin,
+                        'end': end,
+                        'duration': duration,
+                        'x': x,
+                        'y': y,
+                        'width':width,
+                        'height':height,
+                        'fontSize':fontSize,
+                        'color':color,
+                        'strokeColor':strokeColor,
+                        'writing-mode':writing_mode,
+                        'rotate': rotate,
+                        'subtitle': subtitle
+                    });
+                    initP();
+                } else if (d.length === 3 && !flag) {
+                    console.log('Time', d);
+                    var minute;
+                    var second;
+
+                    minute = parseInt(d[0][3]) * 10 + parseInt(d[0][4]);
+                    second = parseInt(d[0][6]) * 10 + parseInt(d[0][7]);
+                    begin = minute * 60 + second + parseInt(d[1][0]) / 10.0 + parseInt(d[1][1]) / 100.0;
+
+                    minute = parseInt(d[1][11]) * 10 + parseInt(d[1][12]);
+                    second = parseInt(d[1][14]) * 10 + parseInt(d[1][15]);
+                    end = minute * 60 + second + parseInt(d[2][0]) / 10.0 + parseInt(d[2][1]) / 100.0;
+                    flag = !flag;
+                    console.log(end);
+                } else if (d.length > 3 && !isNaN(begin)) {
+                    x=d[0];
+                    y=parseInt(d[1]);
+                    width=parseInt(d[2]);
+                    height=parseInt(d[3]);
+                    fontSize=parseInt(d[4]);
+                    color=d[5];
+                    strokeColor=d[6];
+                    writing_mode=parseInt(d[7]);
+                    duration = end - begin;
+                    if(d.length> 8){
+                        rotate = {
+                            'degree': d[8]                        }
+                    }
+                } else {
+                    if (subtitle === '') 
+                        subtitle = d[0];
+                }
+            });
+    });
+}
+
+function playGD(width, height, currentTime){
+    console.log('GD');
+    console.log(SN);
+    console.log(verse);
+    console.log(chorus);
+    d3.select('#SN')
+        .attr('width', width)
+        .attr('height', height)
+        .selectAll('text')
+        .data(SN.concat(verse).concat(chorus))
+        .enter()
+        .append('text')
+        .text(function(d) {
+            return d.subtitle;
+        })
+        .attr('x', function(d) {
+            return d.x;
+        })
+        .attr('y', function(d) {
+            return d.y;
+        })
+        .attr('font-size', function(d) {
+            return d.fontSize;
+        })
+        .attr('transform',function(d){
+            if(!isEmpty(d.rotate)){
+                console.log('rotate',d);
+                return 'rotate('+ d.rotate['degree'] +','+ $(this).attr('x') +','+ $(this).attr('y') +')';
+            }
+            return '';
+        })
+        .attr('fill', function(d){ return d.color})
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline','central')
+        .attr('stroke', function(d){ return d.strokeColor})
+        .attr('stroke-width','1px')
+        .attr('writing-mode',function(d){
+            if(d['writing-mode']==1){
+                return 'tb';
+            }
+            return '';
+        })
+        .attr('opacity', 0)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime)
+                return 0;
+            return (d.begin - currentTime) * 1000;
+        })
+        .duration(0)
+        .attr('opacity', 1)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime && d.end > currentTime)
+                return (d.end - currentTime)*1000;
+            else if(currentTime <= d.begin)
+                return d.duration * 1000;
+            else
+                return 0;
+        })
+        .attr('opacity', 0);
+}
--- lab09/src/index.js
+++ lab09/src/index.js
@@ -1,11 +1,93 @@
 var cm;
 var dp;
 
-var myArray = [];
+var myArray = new Array();
+var Ricks = new Array();
 var Rick = [3,4,5,6,11,13];
 var leftArray = [];
 var rightArray = [];
 var FontSize = 30;
+var sub1 = [];
+
+function playSub(svgID, texts, width, height, fontSize, currentTime=0){
+    d3.select('#'+ svgID)
+        .attr('width', width)
+        .attr('height', height)
+        .selectAll('text')
+        .data(texts)
+        .enter()
+        .append('text')
+        .text(function(d) {
+            return d.subtitle;
+        })
+        .attr('x', width / 2)
+        .attr('y', height/2)
+        .attr('font-size', fontSize)
+        .attr('fill', function(d){ return d.color})
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline','central')
+        .attr('stroke', function(d){ return d.strokeColor})
+        .attr('stroke-width','1px')
+        .attr('opacity', 0)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime)
+                return 0;
+            return (d.begin - currentTime) * 1000;
+        })
+        .duration(0)
+        .attr('opacity', 1)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime && d.end > currentTime)
+                return (d.end - currentTime)*1000;
+            else if(currentTime <= d.begin)
+                return d.duration * 1000;
+            else
+                return 0;
+        })
+        .attr('opacity', 0);
+}
+
+function playOrgSub(svgID, texts, width, height, fontSize, currentTime=0){
+    d3.select('#'+ svgID)
+        .attr('width', width)
+        .attr('height', height)
+        .selectAll('text')
+        .data(texts)
+        .enter()
+        .append('text')
+        .text(function(d) {
+            return d.orgsubtitle;
+        })
+        .attr('x', width / 2)
+        .attr('y', height/2)
+        .attr('font-size', fontSize)
+        .attr('fill', function(d){ return d.color})
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline','middle')
+        .attr('stroke', function(d){ return d.strokeColor})
+        .attr('stroke-width','1px')
+        .attr('opacity', 0)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime)
+                return 0;
+            return (d.begin - currentTime) * 1000;
+        })
+        .duration(0)
+        .attr('opacity', 1)
+        .transition()
+        .delay(function(d) {
+            if(d.begin < currentTime && d.end > currentTime)
+                return (d.end - currentTime)*1000;
+            else if(currentTime <= d.begin)
+                return d.duration * 1000;
+            else
+                return 0;
+        })
+        .attr('opacity', 0);
+}
 
 function readData(Arr, src){
     d3.text(src).then(function(data) {
@@ -56,272 +138,94 @@ function readData(Arr, src){
             });
     });
 }
-function play(){
-    $("svg").empty();
-    let height = 40;
-    let width = $('#video').width();
-    // $('#mixU').attr('viewBox', '0 0 '+width+' '+height+'px');
-    playGrandBlue(width, height, this.currentTime);
-}
 //init
 $(function(){
     readData(myArray, './src/grand_blue.srt');
+    readData(sub1, './src/Sincerely.srt');
     console.log(myArray);
+    console.log(sub1);
     readData(leftArray, './src/left.srt');
     readData(rightArray,'./src/right.srt')
     console.log(leftArray);
-    $("#video").on("play seeked loadeddata", play);
-    $("#video").on("pause",function(e){
+
+    read(SN, './src/Goodbye Declaration/SongName.srt');
+    read(verse, './src/Goodbye Declaration/verse.srt');
+    read(chorus,'./src/Goodbye Declaration/chorus.srt')
+    $("#video").on("play seeked", function(e){
+        $(this).closest('.container').find('svg').empty();
+        let height = $(this).closest(".container").find('.overlay').height() / 2;
+        let width = $('#video').width();
+        playGrandBlue(width, height, this.currentTime);
+    });
+    $("#video, #video1").on("pause",function(e){
         console.log("pause");
-        $("svg").empty();
+        $(this).closest('.container').find('svg').empty();
     })
+    $("#video1").on("play seeked", function(e){
+        $(this).closest('.container').find('svg').empty();
+        let height = $(this).closest(".container").find('.overlay').height() / 2;
+        let width = $('#video1').width();
+        playSincerely(width, height, this.currentTime);
+    });
+    $("#video2").on("play seeked", function(e){
+        $(this).closest('.container').find('svg').empty();
+        $(this).closest('.container').find('.overlayAll').css("display","");
+        let height = $(this).height();
+        let width = $(this).width();
+        playGD(width, height, this.currentTime);
+    });
+    $("#video2").on("pause", function(e){
+        $(this).closest('.container').find('svg').empty();
+        $(this).closest('.container').find('.overlayAll').css("display","none");
+    });
 });
 
 
 function playGrandBlue(width, height, currentTime) { 
     console.log(currentTime)
+    if( myArray.length !==0 && rightArray.length !==0 && leftArray.length !==0){
+        for(let i=myArray.length-1;i>=0;i--){
+            myArray[i]['color'] = 'white';
+            if(Rick.includes(i))
+                myArray[i]['strokeColor'] = '#6F4E37';
+            else
+                myArray[i]['strokeColor'] = '#29799e';
+        }
+        for(let i=rightArray.length-1;i>=0;i--){
+            rightArray[i]['color'] = 'white';
+            rightArray[i]['strokeColor'] = '#29799e';
+        }
+        for(let i=leftArray.length-1;i>=0;i--){
+            leftArray[i]['color'] = 'white';
+            leftArray[i]['strokeColor'] = '#6F4E37';
+        }
+    }
     d3.select('#sperate')
         .style('display', 'none')
-        d3.select('#mix')
+    d3.select('#mix')
         .style('display', '')
 
-    d3.select('#mixU')
-        .selectAll('text')
-        .data(myArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.orgsubtitle;
-        })
-        .attr('x', width / 2)
-        .attr('y', height/2)
-        .attr('font-size', FontSize+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke',function(d){
-            if(Rick.includes(d.key))
-                return '#6F4E37';
-            return '#29799e';
-        })
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
-
-    d3.select('#mixD')
-        .selectAll('text')
-        .data(myArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.subtitle;
-        })
-        .attr('x', width / 2)
-        .attr('y', height/2)
-        .attr('font-size', FontSize+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke',function(d){
-            if(Rick.includes(d.key))
-                return '#6F4E37';
-            return '#29799e';
-        })
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
-
-        d3.select('#speRU')
-        .attr('width', width/2)
-        .selectAll('text')
-        .data(rightArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.orgsubtitle;
-        })
-        .attr('x', width / 4)
-        .attr('y', height/2)
-        .attr('font-size', FontSize/2+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke','#29799e')
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
-
-        d3.select('#speRD')
-        .attr('width', width/2)
-        .selectAll('text')
-        .data(rightArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.subtitle;
-        })
-        .attr('x', width / 4)
-        .attr('y', height/2)
-        .attr('font-size', FontSize/2+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke','#29799e')
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
-
-        d3.select('#speLU')
-        .attr('width', width/2)
-        .selectAll('text')
-        .data(leftArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.orgsubtitle;
-        })
-        .attr('x', width / 4)
-        .attr('y', height/2)
-        .attr('font-size', FontSize/2+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke','#6F4E37')
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
-
-        d3.select('#speLD')
-        .attr('width', width/2)
-        .selectAll('text')
-        .data(leftArray)
-        .enter()
-        .append('text')
-        .text(function(d) {
-            return d.subtitle;
-        })
-        .attr('x', width / 4)
-        .attr('y', height/2)
-        .attr('font-size', FontSize/2+'pt')
-        .attr('fill', 'white')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline','central')
-        .attr('stroke','#6F4E37')
-        .attr('stroke-width','1px')
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime)
-                return 0;
-            return (d.begin - currentTime) * 1000;
-        })
-        .duration(0)
-        .attr('opacity', 1)
-        .transition()
-        .delay(function(d) {
-            if(d.begin < currentTime && d.end > currentTime)
-                return (d.end - currentTime)*1000;
-            else if(currentTime <= d.begin)
-                return d.duration * 1000;
-            else
-                return 0;
-        })
-        .attr('opacity', 0);
+    playOrgSub('mixU', myArray, width, height, height*0.8, currentTime);
+    playSub('mixD', myArray, width, height, height*0.8, currentTime);
+    playOrgSub('mixU', Ricks, width, height, height*0.8, currentTime);
+    playSub('mixD', Ricks, width, height, height*0.8, currentTime);
+    playOrgSub('speRU', rightArray, width/2, height/2, height*0.8/2, currentTime);
+    playSub('speRD', rightArray, width/2, height/2, height*0.8/2, currentTime);
+    playOrgSub('speLU', leftArray, width/2, height/2, height*0.8/2, currentTime);
+    playSub('speLD', leftArray, width/2, height/2, height*0.8/2, currentTime);
     
     d3.select('#mix')
         .transition()
         .delay(function(){
+            console.log(currentTime > myArray[myArray.length-1].end)
             if(currentTime > myArray[myArray.length-1].end)
                 return 0;
+            console.log(currentTime > myArray[myArray.length-1].end * 1000)
             return (myArray[myArray.length-1].end - currentTime) * 1000
         })
         .style('display', 'none')
     d3.select('#sperate')
+        .style('width', width+'px')
         .transition()
         .delay(function(){
             if(currentTime > myArray[myArray.length-1].end)
@@ -329,4 +233,17 @@ function playGrandBlue(width, height, currentTime) {
             return (myArray[myArray.length-1].end - currentTime) * 1000
         })
         .style('display', '');
+}
+
+function playSincerely(width, height, currentTime){
+    if(sub1.length!==0){
+        for(let i=sub1.length-1;i>=0;i--){
+            sub1[i]['color'] = 'white';
+            sub1[i]['strokeColor'] = '#f6e9cc';
+        }
+        console.log(sub1);
     }
+    playOrgSub('sub1U', sub1, width, height, FontSize, currentTime);
+    playSub('sub1D', sub1, width, height, FontSize, currentTime);
+}
+
